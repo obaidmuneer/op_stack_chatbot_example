7225dc60c5d5fc1b389f307069ca2ddd7e69c02f
obaidmuneer <obaidmuneer55@gmail.com>
2023-07-23 23:44:58 +0500

    rename file

--- helper/pdf.mjs
+++ helper/pdf.mjs
@@ -1,7 +1,12 @@
+import { create_chunks } from "./chunk.mjs";
+import { inser_multi_vectors } from "./pinecone.mjs";
 import PDFParser from "pdf2json";
+import { resolve, join } from 'path'
+
+const __dirname = resolve()
 
 // https://www.npmjs.com/package/pdf2json
-export const pdf_to_text = (pathToFile) => {
+export const pdf_to_text = (file_name) => {
     const pdfParser = new PDFParser(this, 1);
 
     return new Promise((resolve, reject) => {
@@ -10,8 +15,8 @@ export const pdf_to_text = (pathToFile) => {
             // console.log(pdfData);
             resolve(pdfParser.getRawTextContent())
         });
-        pdfParser.loadPDF(pathToFile);
+        pdfParser.loadPDF(join(__dirname, `data/${file_name}`));
     })
 }
 
-// console.log(await pdf_to_text('./data/obaidmuneer.pdf'));
+// console.log(await pdf_to_text('obaidmuneer.pdf'));
